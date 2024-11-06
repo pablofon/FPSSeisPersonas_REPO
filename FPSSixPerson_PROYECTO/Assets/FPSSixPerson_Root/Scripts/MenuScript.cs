d5deebb575c89766dcf407fa8947cc940d015eee
ph3lb0t <eldior05@gmail.com>
2024-11-06 19:18:01 +0100

    game over done

---
+++ FPSSixPerson_PROYECTO/Assets/FPSSixPerson_Root/Scripts/MenuScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Retrurn()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
