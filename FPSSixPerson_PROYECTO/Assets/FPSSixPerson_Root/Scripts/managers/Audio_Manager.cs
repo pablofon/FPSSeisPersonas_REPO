99f0dcde0425d2e81fd34c5c8ef803128c7ff715
ph3lb0t <eldior05@gmail.com>
2024-11-15 19:07:09 +0100

    a

--- FPSSixPerson_PROYECTO/Assets/FPSSixPerson_Root/Scripts/managers/Audio_Manager.cs
+++ FPSSixPerson_PROYECTO/Assets/FPSSixPerson_Root/Scripts/managers/Audio_Manager.cs
@@ -25,6 +25,7 @@ public class Audio_Manager : MonoBehaviour
     public AudioClip Weapon;
     public AudioClip Vent;
     public AudioClip Walk;
+    public AudioClip Danger;
 
     [Header("_________Voicelines_________")]
 
